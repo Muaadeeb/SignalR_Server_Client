daf4b67e1e8f7cde23b57c7537eb895260edb765
Timothy Ingledue <TimothyIngledue@gmail.com>
2025-08-27 22:36:34 -0500

    #0004 - Changed to use components.

---
+++ SignalR_Server_Client/SignalR/Client/Shared/Models/StatusEntry.cs
@@ -0,0 +1,15 @@
+namespace SignalR.Client.Shared.Models;
+
+public class StatusEntry    
+{
+    public DateTime Timestamp { get; set; } = DateTime.Now;
+    public string Message { get; set; } = string.Empty;
+
+    public StatusEntry() { }
+
+    public StatusEntry(string message)
+    {
+        Timestamp = DateTime.Now;
+        Message = message;
+    }
+}
